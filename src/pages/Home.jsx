65b82f827991822e8f67a6d6b011d1784eb568c5
John Beltrez <beltrezjohn1@gmail.com>
2025-02-13 21:01:22 -0500

    home page created, resolves false import browser error

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to the Bar Menu Launcher</h1>
+      <p>Build and manage your custom bar menus with ease. Navigate through drinks and menus to start creating your perfect lineup.</p>
+    </div>
+  );
+};
+
+export default Home;
+
